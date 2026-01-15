cd74462b131a3dc6f565b55d1d7aaa4285ec161c
MateiTita <matei.tita@stud.fils.upb.ro>
2026-01-16 01:08:10 +0200

    added illumination like in lab 9

--- GameEngine/Shaders/fragment_shader.glsl
+++ GameEngine/Shaders/fragment_shader.glsl
@@ -13,7 +13,34 @@ uniform vec3 viewPos;
 
 void main()
 {
-	//TO DO: Add illumination from Lab 9
+	// ============ AMBIENT ============
+	float ka = 0.1;  
+	vec3 ambient = ka * lightColor;
 
-	fragColor = texture(texture1, textureCoord);
+	// ============ DIFFUSE ============
+	
+	vec3 normalVec = normalize(norm);
+	vec3 lightDir = normalize(lightPos - fragPos);
+	
+	float kd = 1.0;  // diffuse coefficient of the material
+	float diff = max(dot(normalVec, lightDir), 0.0);
+	vec3 diffuse = kd * diff * lightColor;
+
+	// ============ SPECULAR ============
+	
+	float ks = 0.5;  
+	
+	vec3 viewDir = normalize(viewPos - fragPos);
+	vec3 reflectDir = reflect(-lightDir, normalVec);
+	
+	float shininess = 32.0;
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0), shininess);
+	vec3 specular = ks * spec * lightColor;
+
+	// ============ COMBINE ALL ============
+	vec3 lighting = ambient + diffuse + specular;
+	vec4 objectColor = texture(texture1, textureCoord);
+	vec3 result = lighting * objectColor.rgb;
+
+	fragColor = vec4(result, objectColor.a);
 }
